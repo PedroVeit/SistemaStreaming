905612f51d08ceca885a0e20e0f2a129b9d6a7a8
Gustavo Silveira Goulart <gustavosilveira780@gmail.com>
2025-10-08 10:18:50 -0300

    correção da playlist.py

--- Streaming/playlist.py
+++ Streaming/playlist.py
@@ -2,31 +2,28 @@ from .arquivo_de_midia import ArquivoDeMidia
 
 
 class Playlist:
-    def __init__(self, nome, usuario):
+      reproducoes = 0
+    
+    def __init__(self, nome: str, usuario, iens: list[ArquivoDeMidia]):
         self.nome = nome
         self.usuario = usuario
-        self.itens = []
-        self.reproducoes = 0
+        self.itens = itens
+      
 
-    def adicionar_midia(self, midia):
+    def adicionar_midia(self, midia: ArquivoDeMidia):
         self.itens.append(midia)
 
-    def remover_midia(self, midia):
-        try:
-            self.itens.remove(midia)
-        except ValueError:
-            pass
+    def remover_midia(self, midia: ArquivoDeMidia):
+        self.itens.append(midia)
 
     def reproduzir(self):
         self.reproducoes += 1
-
-        for midia in self.itens:
-            midia.reproduzir()
+        print('Músicas reproduzidas')
+        for i in self.itens:
+            print(i)
+            i.reproducoes += 1
 
     def __add__(self, other):
-        #if not isinstance(other, Playlist):
-            #return ValueError
-        
         nova = Playlist(self.nome, self.usuario)
         nova.itens = list(self.itens) + list(other.itens)
         nova.reproducoes = self.reproducoes + other.reproducoes
@@ -41,14 +38,5 @@ class Playlist:
     def __eq__(self, other):
         if not isinstance(other, Playlist):
             return False
-        
-        mesmos = [m.titulo for m in self.itens] == [m.titulo for m in other.itens]
-        return self.nome == other.nome and self.usuario == other.usuario and mesmos
+        (self.nome == other.nome and self.usario == other.usuario and self.itens == other.itens)
 
-    #def __repr__(self):
-        #return "Playlist(nome='%s', usuario=%s, itens=%d, reproducoes=%d)" % (
-            #self.nome, self.usuario, len(self.itens), self.reproducoes
-        #)
-
-    #def __str__(self):
-        #return "Playlist '%s' por %s (%d itens)" % (self.nome, self.usuario.nome, len(self))
