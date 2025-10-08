3224dc81c621f1940781adb67bd57d1e515750fc
Gustavo Silveira Goulart <gustavosilveira780@gmail.com>
2025-10-08 10:43:00 -0300

    Update arquivo_de_midia.py

--- Streaming/arquivo_de_midia.py
+++ Streaming/arquivo_de_midia.py
@@ -8,8 +8,6 @@ class ArquivoDeMidia:
         self.artista = artista
         self.reproducoes = 0
 
-        self.likes = 0 #implementação de curtidas
-
     def reproduzir(self):
         return "%s(titulo='%s', duracao=%d, artista='%s', reproducoes=%d)" % (self.__class__.__name__, self.titulo, self.duracao, self.artista, self.reproducoes)
     
